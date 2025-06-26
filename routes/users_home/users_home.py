5f73f1ea4c6a4c5ab0bf840e048f02bdf83d76bb
HARUKITERASHIMA <s2422049@stu.musashino-u.ac.jp.com>
2025-06-27 03:38:43 +0900

    ホーム画面多分できた

--- routes/users_home/users_home.py
+++ routes/users_home/users_home.py
@@ -1,8 +1,8 @@
-# routes/general/explamation.py
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, session, redirect, url_for, flash
 
+# Blueprint作成
 users_home_bp = Blueprint('users_home', __name__, url_prefix='/users_home')
 
-@users_home_bp.route('/users_home')
-def users_home():
+@users_home_bp.route('/home')
+def home():
     return render_template('users_home/home.html')
