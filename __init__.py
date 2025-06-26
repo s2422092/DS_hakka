9b9e86a4a6a6ed89e525def1e1399c639eab755b
otsukaiora <s2422104@stu.musashino-u.ac.jp>
2025-06-27 05:32:18 +0900

    見えるようになった

--- __init__.py
+++ __init__.py
@@ -7,7 +7,7 @@ def create_app():
     # ルートの登録
     from routes.general.explamation import general_bp
     from routes.stores.store import store_bp 
-    from routes.stores_detail.stores_detail import store_detail_bp
+    from routes.stores_detail.stores_detail import stores_detail_bp
     from routes.users_home.users_home import users_home_bp
     from routes.users_login.users_login import users_login_bp
     from routes.users_order.users_order import users_order_bp
@@ -15,7 +15,7 @@ def create_app():
 
     app.register_blueprint(general_bp)
     app.register_blueprint(store_bp)
-    app.register_blueprint(store_detail_bp)
+    app.register_blueprint(stores_detail_bp)
     app.register_blueprint(users_home_bp)
     app.register_blueprint(users_login_bp)
     app.register_blueprint(users_order_bp)
--- routes/stores_detail/stores_detail.py
+++ routes/stores_detail/stores_detail.py
@@ -1,8 +1,24 @@
 # routes/general/explamation.py
 from flask import Blueprint, render_template
 
-store_detail_bp = Blueprint('store_detail', __name__, url_prefix='/store_detail')
+stores_detail_bp = Blueprint('stores_detail', __name__, url_prefix='/stores_detail')
 
-@store_detail_bp.route('/store_detail')
-def store_detail():
-    return render_template('store_detail/menu_check.html')
+@stores_detail_bp.route('/menu_check')
+def menu_check():
+    return render_template('stores_detail/menu_check.html')
+
+@stores_detail_bp.route('/menu_registration')
+def menu_registration():
+    return render_template('stores_detail/menu_registration.html')
+
+@stores_detail_bp.route('/order_list')
+def order_list():
+    return render_template('stores_detail/order_list.html')
+
+@stores_detail_bp.route('/procedure')
+def procedure():
+    return render_template('stores_detail/procedure.html')
+
+@stores_detail_bp.route('/store_home')
+def store_home():
+    return render_template('stores_detail/store_home.html')
