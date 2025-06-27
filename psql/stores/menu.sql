40d6cce7f1cdb526556fb12e5b0e78069777f73b
南保 俊輔 <166090357+s2422051@users.noreply.github.com>
2025-06-27 12:45:45 +0900

    menusテーブルぶち込み完了

--- psql/stores/menu.sql
+++ psql/stores/menu.sql
@@ -4,7 +4,10 @@ CREATE TABLE menus (
     menu_name TEXT NOT NULL,
     category  TEXT,
     price     INT NOT NULL,
-    soldout   INT NOT NULL DEFAULT 0, -- 在庫ありを0、売り切れを1とします。デフォルトは在庫あり(0)。
-    PRIMARY KEY (menu_id), -- menu_idを主キーに設定
-    FOREIGN KEY (store_id) REFERENCES stores(store_id) -- store_idを外部キーとして、storesテーブルのstore_idに関連付け
+    soldout   INT NOT NULL DEFAULT 0, 
+    PRIMARY KEY (menu_id), 
+    FOREIGN KEY (store_id) REFERENCES stores(store_id) 
 );
+
+menu_idを主キーに設定
+-- store_idを外部キーとして、storesテーブルのstore_idに関連付け
