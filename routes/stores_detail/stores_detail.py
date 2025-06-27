e673033e02f1bf3d3c272b35c5b1df6c4844e32d
s2422092 <2422092@stu.musashino-u.ac.jp>
2025-06-27 12:59:47 +0900

    ストアログインの完成。そしてsessionの機能を確立させた

--- routes/stores_detail/stores_detail.py
+++ routes/stores_detail/stores_detail.py
@@ -1,35 +1,6 @@
-# # routes/general/explamation.py
-# from flask import Blueprint, render_template
 
-# stores_detail_bp = Blueprint('stores_detail', __name__, url_prefix='/stores_detail')
-
-# @stores_detail_bp.route('/menu_check')
-# def menu_check():
-#     return render_template('stores_detail/menu_check.html')
-
-# @stores_detail_bp.route('/menu_registration')
-# def menu_registration():
-#     return render_template('stores_detail/menu_registration.html')
-
-# @stores_detail_bp.route('/order_list')
-# def order_list():
-#     return render_template('stores_detail/order_list.html')
-
-# @stores_detail_bp.route('/procedure')
-# def procedure():
-#     return render_template('stores_detail/procedure.html')
-
-# @stores_detail_bp.route('/store_home')
-# def store_home():
-#     return render_template('stores_detail/store_home.html')
-
-# routes/stores_detail.py
-
-# routes/stores_detail.py
-
-# routes/stores_detail.py
-
-from flask import Blueprint, render_template, request, redirect, url_for
+from flask import Blueprint, render_template, request, redirect, url_for, flash, session
+import sqlite3
 
 # Blueprintを定義
 # url_prefixは、これらのページのURLがすべて /stores から始まることを意味します
@@ -40,8 +11,13 @@ stores_detail_bp = Blueprint('stores_detail', __name__, url_prefix='/stores')
 # /stores/home にアクセスされた時に呼ばれる
 @stores_detail_bp.route('/home')
 def store_home():
-    # templates/stores_detail/store_home.html を表示
-    return render_template('stores_detail/store_home.html')
+    if 'store_id' not in session:
+        flash("ログインしてください")
+        return redirect(url_for('store.store_login'))
+
+    store_name = session.get('store_name', '未登録')
+    return render_template('stores_detail/store_home.html', store_name=store_name)
+
 
 # /stores/menu-registration にアクセスされた時に呼ばれる
 @stores_detail_bp.route('/menu-registration', methods=['GET', 'POST'])
